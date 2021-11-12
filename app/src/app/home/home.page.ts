d5440afbef0a05a43aab6ff26fca9046e01a4dfb
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2021-11-12 21:08:27 +0100

    set up project and added map

---
+++ app/src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 47.12772;
+  lng = 8.31589;
+  zoom = 12;
+  accessToken = environment.mapboxkey;
+  data: any;
+  geojson: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: this.accessToken,
+      container: 'fullscreen',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat]});
+
+      this.map.on('load', () => {
+        this.map.resize();
+    });
+  }
+
+}
